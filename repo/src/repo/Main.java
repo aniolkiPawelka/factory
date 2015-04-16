97aa278bec4722369bd882bf0018bb0fb48f2f8f
sdf <sdfsd@oonet.pl>
2015-04-16 10:35:24 +0200

    zmienilem :-p

--- repo/src/repo/Main.java
+++ repo/src/repo/Main.java
@@ -2,7 +2,7 @@ package repo;
 
 public class Main {
 	public static void klasa(){
-	System.out.println("Piotr i Mateusz to miszcze");
+	System.out.println("Piotr i Mateusz to miszczeeeeeeee a Michal i Maciek nie !!!!!!!!");
 	}
 
 }
