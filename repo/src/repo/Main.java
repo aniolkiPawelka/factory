86a38c96db5e7bb1ac02b75e78c53a5217ae886f
admin <admin@wp.pl>
2015-04-16 09:53:25 +0200

    .hj

--- repo/src/repo/Main.java
+++ repo/src/repo/Main.java
@@ -1,5 +1,8 @@
 package repo;
 
 public class Main {
+	public static void klasa(){
+	System.out.println("Piotr i Mateusz to miszcze");
+	}
 
 }
---
+++ repo/src/repo/start.java
@@ -0,0 +1,10 @@
+package repo;
+
+public class start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main.klasa();
+	}
+
+}
